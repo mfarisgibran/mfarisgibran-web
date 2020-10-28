235ecfcdf3519b41e44140b7f01f4b2ba8489e32
M Haidar Hanif <git@mhaidarhanif.com>
2020-10-28 23:26:23 +0700

    Use modified airtable browser

--- contact/contact.js
+++ contact/contact.js
@@ -1,35 +1,32 @@
-const Airtable = require("airtable");
+var Airtable = require('airtable')
+var base = new Airtable({ apiKey: 'keyVmZRFl4GrOMtGS' }).base(
+  'appbqdLTu3TWgAupV'
+)
 
-const base = new Airtable({ apiKey: "keyVmZRFl4GrOMtGS" }).base(
-  "appbqdLTu3TWgAupV"
-);
+const contactFormElement = document.getElementById('contact-form')
 
-const contactFormElement = document.getElementById("contact-form");
+contactFormElement.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-contactFormElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const contactNameElement = document.getElementById("contact-name");
-  const contactEmailElement = document.getElementById("contact-email");
-  const contactPhoneElement = document.getElementById("contact-phone");
-  const contactSubjectElement = document.getElementById("contact-subject");
-  const contactMessageElement = document.getElementById("contact-message");
+  const contactNameElement = document.getElementById('contact-name')
+  const contactEmailElement = document.getElementById('contact-email')
+  const contactPhoneElement = document.getElementById('contact-phone')
+  const contactSubjectElement = document.getElementById('contact-subject')
+  const contactMessageElement = document.getElementById('contact-message')
 
   const contactData = {
     name: contactNameElement.value,
     email: contactEmailElement.value,
     phone: contactPhoneElement.value,
     subject: contactSubjectElement.value,
-    message: contactMessageElement.value,
-  };
+    message: contactMessageElement.value
+  }
 
-  base("Contact").create([contactData], (err, records) => {
+  base('Contact').create(contactData, (err, record) => {
     if (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
-    records.forEach(function (record) {
-      console.log(record.getId());
-    });
-  });
-});
+    console.log(record)
+  })
+})
